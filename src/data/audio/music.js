5a6cb77bf40c6df1bb3b8864234bc570a44bf8fe
Black Jack <hibbarcd@gmail.com>
2019-05-09 15:37:03 -0500

    adding audio patch, still non functional

--- src/data/audio/music.js
+++ src/data/audio/music.js
@@ -1,24 +1,48 @@
-import store from '../../config/store'
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default function handleMusic(player) {
-    
+ class AudioController {
+   constructor() {
+
     this.bgMusic = new Audio('Adventure.mp3');
     this.bgMusic.volume = 0.4;
     this.bgMusic.loop = true;
- }
-componentDidMount() {
-   const audio = new Audio();
-   audio.src = "Adventure.mp3"
+   }
+   
+ startMusic() {
+  this.bgMusic = new Audio();
+  console.log('audio playing')
+  }
+
+stopMusic() {  
+  console.log('audio stopped')
+  }
 }
 
-playSong(audio) {  
-  console.log('audio playing')
-  audio.play();
+class AudioSetup {
+  constructor() {
+    this.audioController = new AudioController()
+  }
+
+  autoplayAudio() {
+    this.audioController.startMusic()
+  }
+}
+if ( document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', ReadyMusic());
+}
+else {
+  ReadyMusic();
+}
+
+function ReadyMusic(props) {
+  return ( AudioSetup.readyMusic()
+  )
 }
 
 function mapStateToProps(state) {
-    return {
-      ...state.player,
-    }
+  return {
+    ...state.player,
   }
 }
+export default connect(mapStateToProps)(ReadyMusic)
